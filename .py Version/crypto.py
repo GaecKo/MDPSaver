a33374c8604be561ca24da3aefe0be41a2fc7f2c
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2022-08-08 19:32:57 +0200

    easier and more secure hash function
    	modified:   crypto.py

--- .py Version/crypto.py
+++ .py Version/crypto.py
@@ -43,37 +43,6 @@ def decode(key, cipher_text):
 		dec.append(dec_c)
 	return str("".join(dec))
 
-def hashing(string):
-	"""
-	Hachage d'une chaîne de caractères fournie en paramètre.
-	Le résultat est une chaîne de caractères.
-	Attention : cette technique de hachage n'est pas suffisante (hachage dit cryptographique) pour une utilisation en dehors du cours.
+def hashing(to_hash: str) -> str:
+    return str(hash(to_hash))
 
-	:param (str) string: la chaîne de caractères à hacher
-	:return (str): le résultat du hachage
-	"""
-	def to_32(value):
-		"""
-		Fonction interne utilisée par hashing.
-		Convertit une valeur en un entier signé de 32 bits.
-		Si 'value' est un entier plus grand que 2 ** 31, il sera tronqué.
-
-		:param (int) value: valeur du caractère transformé par la valeur de hachage de cette itération
-		:return (int): entier signé de 32 bits représentant 'value'
-		"""
-		value = value % (2 ** 32)
-		if value >= 2**31:
-			value = value - 2 ** 32
-		value = int(value)
-		return value
-
-	if string:
-		x = ord(string[0]) << 7
-		m = 1000003
-		for c in string:
-			x = to_32((x*m) ^ ord(c))
-		x ^= len(string)
-		if x == -1:
-			x = -2
-		return str(x)
-	return ""

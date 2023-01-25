47368e24eb4b689cf4f7d5430850fb67607da199
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-01-25 11:42:22 +0100

    dev branch is up, updated todo 🚧

---
+++ todo.py
@@ -0,0 +1,20 @@
+# TODO: README.md 
+# TODO: param func
+# Create TODO app
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# SQLite as the database
+# More functionnalities: search by email, username, rework of the password menu
+
+
+# === DONE ===
+# TODO: generate password
+# TODO: repaired recovery system
+# TODO: add 200 lines after generating password
+# TODO: Hide AP
+# TODO: color first logo
+# TODO: system settings logs
+# TODO: bug changer mdp bad pas
+# TODO: mdp not hidden when reentering it after changing
+# TODO: mdp not hidden when confirming it in system settings
+# TODO: color add generated password 
+# TODO: COLOR

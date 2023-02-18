e98f313eb4f56f9691de81a065f50b1d6cb471a2
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-02-18 20:33:17 +0100

    Reworked style of the SpeedTest

--- MDP_APP/SpeedTest.py
+++ MDP_APP/SpeedTest.py
@@ -34,49 +34,30 @@ def load_data():
     with open("SpeedTest.txt", 'r', encoding="utf-8") as f:
         content = f.readlines()
     for i in range(len(content)-1):
-        print(decrypt(pr.get_key(), content[i].rstrip("\n")))
+        decrypt(pr.get_key(), content[i].rstrip("\n"))
 
 def calcul_time(nbr_password:int) -> tuple:
     a = time.time()
     generate_password(nbr_password)
     b = time.time()
-    print(Fore.GREEN + f" ~> Done [1/2] ({str(round(b-a))} sec)")
+    print(Fore.GREEN + f" ~> Done [1/2] (Encryption: {str(round(b-a, 5))} sec)")
     load_data()
     c = time.time()
-    print(Fore.GREEN + f" ~> Done [2/2] ({str(round(c-b))} sec)")
+    print(Fore.GREEN + f" ~> Done [2/2] (Decryption: {str(round(c-b, 5))} sec)")
     del_file()
     return b-a, c-b
-
-def animation(loadingtext):
-    word = list(loadingtext)
-    for i in range(0,len(word)):
-        lower=word[i-1].lower()
-        word[i-1]=lower
-        caps=word[i].upper()
-        word[i]=caps
-        wordstr=''.join(word)
-        sys.stdout.write('\r' + wordstr)
-        time.sleep(0.3)
 
 def check_key():
     with open("MDPCrypto/key/key.key", 'rb') as f:
         if f.read() == b'':
             generate_salt()
 
-def start_anim(txt):
-    ev = threading.Event()
-    def _loop(ev, txt):
-        while not ev.is_set():
-            animation(txt)
-    threading.Thread(target=_loop, args=(ev, txt)).start()
-    return ev
-
 def write_result(create, load, nbr_password):
     print(
 f"""
 = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-{Fore.GREEN + "Creation" + Style.RESET_ALL} of the {Fore.YELLOW + str(nbr_password) + Style.RESET_ALL} encrypted passwords: {Back.RED + Fore.WHITE + str(round(create, 2)) + Style.RESET_ALL} sec. ~~> ± {Fore.CYAN + str(round((create/nbr_password) * 1000, 3)) + Style.RESET_ALL} ms per password.
-{Fore.BLUE + "Loading" + Style.RESET_ALL} of the {Fore.YELLOW + str(nbr_password) + Style.RESET_ALL} encrypted passwords: {Back.RED + Fore.WHITE + str(round(load, 2)) + Style.RESET_ALL} sec. ~~> ± {Fore.CYAN + str(round((load/nbr_password) * 1000, 3)) + Style.RESET_ALL} ms per password.
+{Fore.GREEN + "Creation" + Style.RESET_ALL} of the {Fore.YELLOW + str(nbr_password) + Style.RESET_ALL} encrypted passwords: {Back.RED + Fore.WHITE + str(round(create, 4)) + Style.RESET_ALL} sec. ~> ± {Fore.CYAN + str(round((create/nbr_password) * 1000, 5)) + Style.RESET_ALL} ms per password.
+{Fore.BLUE + "Loading" + Style.RESET_ALL} of the {Fore.YELLOW + str(nbr_password) + Style.RESET_ALL} encrypted passwords: {Back.RED + Fore.WHITE + str(round(load, 4)) + Style.RESET_ALL} sec. ~> ± {Fore.CYAN + str(round((load/nbr_password) * 1000, 5)) + Style.RESET_ALL} ms per password.
 Total time: {Fore.RED + str(round(load + create, 2)) + Style.RESET_ALL} sec 
 = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 """)
@@ -93,9 +74,7 @@ if __name__ == "__main__":
         except:
             print(Back.RED + 'Enter a valid number.')
 
-    ev = start_anim(Fore.CYAN + "Loading result" )
     create_time, load_time = calcul_time(nbr)
-    ev.set()
     time.sleep(1)
     write_result(create_time, load_time, nbr)
     

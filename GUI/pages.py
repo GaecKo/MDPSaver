517f0c1ef1373765870e76fc74eb83d4b21e7731
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-04-19 18:06:27 +0200

    Worked on GUI, still so much to do

--- GUI/pages.py
+++ GUI/pages.py
@@ -5,7 +5,7 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
-        self.setWindowTitle("Main Window")
+        self.setWindowTitle("MDPSaver")
         self.setFixedSize(800, 600)
 
         central_widget = QWidget()
@@ -15,41 +15,60 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(layout)
 
         left_panel = QVBoxLayout()
+        left_panel.setObjectName("leftPanel")
         right_panel = QVBoxLayout()
+        right_panel.setObjectName("rightPanel")
 
         layout.addLayout(left_panel)
         layout.addLayout(right_panel)
 
         self.passwords_button = QPushButton("Passwords")
+        self.create_button = QPushButton("Add")
         self.account_button = QPushButton("Account")
         self.settings_button = QPushButton("Settings")
 
         left_panel.addWidget(self.passwords_button)
+        left_panel.addWidget(self.create_button)
         left_panel.addWidget(self.account_button)
         left_panel.addWidget(self.settings_button)
 
         self.stacked_widget = QStackedWidget()
 
-        self.passwords_page = self.create_passwords_page()
+        
+
+        self.passwords_page = self.show_passwords_page()
+        self.create_page = self.create_passwords_page()
         self.account_page = self.create_account_page()
         self.settings_page = self.create_settings_page()
 
         self.stacked_widget.addWidget(self.passwords_page)
+        self.stacked_widget.addWidget(self.create_page)
         self.stacked_widget.addWidget(self.account_page)
         self.stacked_widget.addWidget(self.settings_page)
 
         right_panel.addWidget(self.stacked_widget)
 
         self.passwords_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(0))
-        self.account_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
-        self.settings_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
+        self.create_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+        self.account_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(2))
+        self.settings_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(3))
 
-    def create_passwords_page(self):
+
+
+
+    def show_passwords_page(self):
         passwords_page = QGroupBox("Passwords Page")
         layout = QVBoxLayout()
         layout.addWidget(QLabel("This is the Passwords page."))
         passwords_page.setLayout(layout)
         return passwords_page
+
+    def create_passwords_page(self):
+        create_page = QGroupBox("Create Page")
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("This is the Creation page."))
+        create_page.setLayout(layout)
+        return create_page
 
     def create_account_page(self):
         account_page = QGroupBox("Account Page")
--- GUI/test/main.py
+++ GUI/test/main.py
@@ -1,18 +1,50 @@
-import sys
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication
-from login import LoginPage
-from pages import MainWindow
+# Author: Arthur De Neyer
+# Date: 01/03/2023
+# GitHub: GaecKo/MDPSaver
 
+import sys
+import platform
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide6.QtWidgets import *
+
+from ui_Main_App import Ui_MainWindow # ui file
+
+from ui_functions import * # functions file
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # toggle btn connection
+        self.ui.btn_toggle.clicked.connect(lambda: UIFunctions.toggleMenu(self, 250, True))
+
+        ## PAGES
+        ########################################################################
+
+        # Page 1
+        
+
+        ########################################################################
+        self.show()
+    
+    def connections(self):
+        # Pages connection: 
+        self.ui.btn_page_1.clicked.connect(lambda: self.ui.pages_widget.setCurrentWidget(self.ui.page_1))
+
+        # PAGE 2
+        self.ui.btn_page_2.clicked.connect(lambda: self.ui.pages_widget.setCurrentWidget(self.ui.page_2))
+
+        # PAGE 3
+        self.ui.btn_page_3.clicked.connect(lambda: self.ui.pages_widget.setCurrentWidget(self.ui.page_3))
+
+    def on_start(self):
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyleSheet(open("styles.css").read())
-
-    login_page = LoginPage()
-    main_window = MainWindow()
-
-    login_page.successful_login.connect(main_window.show)
-    login_page.show()
-
+    window = MainWindow()
     sys.exit(app.exec())

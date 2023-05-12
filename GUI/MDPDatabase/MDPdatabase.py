698eff80ccde29a772854dab9765b9a5796e0c07
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-05-12 21:21:30 +0200

    DB: still working on backend / controller

--- GUI/MDPDatabase/MDPdatabase.py
+++ GUI/MDPDatabase/MDPdatabase.py
@@ -2,7 +2,30 @@ import sqlite3
 
 class MDPData:
     def __init__(self):
-        con = sqlite3.connect("MDPdatabase.sqlite")
-        self.cur = con.cursor()
+        self.con = sqlite3.connect("MDPdatabase.sqlite")
+        self.cur = self.con.cursor()
     
-    def add_password(self, site: str, username: str, password: str):
+    def add_password(self, site: str, username: str, crypted_password: str) -> None:
+        self.cur.execute(f"INSERT INTO Password (site, username, password) VALUES (?,?,?)", (site, username, crypted_password))
+        self.con.commit()
+    
+    def get_password_data(self, id: int) -> tuple:
+        """
+        @pre: id -> id of the password 
+        @return: a tuple: (site, username, crypted_password) if id in db
+                 None otherwise
+        """
+        self.cur.execute(f"SELECT site, username, password FROM Password WHERE id = {id}")
+
+        result = self.cur.fetchone()
+
+        if result:
+            site, username, password = result
+            return (site, username, password)
+        else:
+            return None 
+    
+    def get_user
+
+    def close_cursor(self):
+        self.cur.close()
---
+++ GUI/MDPDatabase/controller.py
@@ -0,0 +1,7 @@
+from MDPDatabase import MDPData 
+
+class Controller:
+    def __init__(self): 
+        self.db = MDPData()
+
+     

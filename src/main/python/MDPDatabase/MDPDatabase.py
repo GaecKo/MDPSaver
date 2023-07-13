6422d8aec758cb0a08968358453fa96d700a6b9c
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-07-13 17:13:58 +0200

    BE: started working on multiple user system

---
+++ src/main/python/MDPDatabase/MDPDatabase.py
@@ -0,0 +1,138 @@
+import sqlite3
+import os
+from .ApplySQL import apply_sql
+
+
+class MDPDatabase:
+    def __init__(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+
+        database_path = os.path.join(current_dir, "MDPdatabase.sqlite")
+        self.con = sqlite3.connect(database_path)
+        self.cur = self.con.cursor()
+
+    def add_password(self, site: str, username: str, crypted_password: str) -> None:
+        self.cur.execute(f"INSERT INTO Password (site, username, password) VALUES (?,?,?)",
+                         (site, username, crypted_password))
+        self.con.commit()
+
+    def count(self, table):
+        self.cur.execute(f"SELECT COUNT(*) FROM {table}")
+        return self.cur.fetchone()[0]
+
+    def get_password_data(self, id: int) -> tuple:
+        """
+        @pre: id -> id of the password 
+        @return: a tuple: (site, username, crypted_password) if id in db
+                 None otherwise
+        """
+        self.cur.execute(f"SELECT site, username, password FROM Password WHERE id = {id}")
+
+        result = self.cur.fetchone()
+
+        if result:
+            site, username, password = result
+            return (site, username, password)
+        else:
+            return None
+
+    def get_all_passwords(self):
+        self.cur.execute(f"SELECT site, username, password FROM Password")
+
+        result = self.cur.fetchall()
+        return result
+
+    def get_user(self, attribute: str):
+
+        if attribute not in ["username", "times_connected", "serial_number"]:
+            return None
+
+        self.cur.execute(f"SELECT {attribute} FROM User")
+
+        result = self.cur.fetchone()
+
+        if result:
+            return result[0]
+        else:
+            return None
+
+    def get_user_security(self, attribute: str) -> str:
+        if attribute not in ["username", "hashed_password", "hashed_answer", "encrypted_password", "encrypted_question",
+                             "salt"]:
+            return None
+
+        self.cur.execute(f"SELECT {attribute} FROM UserSecurity")
+
+        result = self.cur.fetchone()
+
+        if result:
+            return result[0]
+        else:
+            return None
+
+    def incr_connections(self) -> None:
+        self.cur.execute("UPDATE User SET times_connected = times_connected + 1")
+        self.con.commit()
+
+    def set_username(self, new_username: str) -> None:
+        self.cur.execute("UPDATE User SET username = ? WHERE id = 1", (new_username,))
+        self.con.commit()
+
+    def set_password(self, new_username, new_site, new_pass, id) -> None:
+        query = "UPDATE Password SET site = ?, username = ?, password = ? WHERE id = ?"
+        values = (new_site, new_username, new_pass, id)
+        self.cur.execute(query, values)
+
+        self.con.commit()
+
+    def is_app_initialized(self):
+
+        self.cur.execute("SELECT username FROM User")
+
+        result = self.cur.fetchone()
+
+        if result:
+            return True
+        else:
+            return False
+
+    def initiate_user(self, username, serial_number, number_of_connections=0) -> bool:
+
+        self.cur.execute(f"INSERT INTO User (username, times_connected, serial_number) VALUES (?, ?, ?)",
+                         (username, number_of_connections, serial_number))
+        try:
+            self.con.commit()
+            return True
+        except:
+            return False
+
+    def delete_user_security(self):
+
+        self.cur.execute(f"DELETE FROM UserSecurity")
+        self.con.commit()
+
+    def delete_user(self):
+
+        self.cur.execute(f"DELETE FROM User")
+        self.con.commit()
+
+    def set_serial_number(self, serial_number):
+        username = self.get_user("username")
+        self.cur.execute(f"UPDATE User SET serial_number WHERE username = {username}")
+
+    def initiate_user_security(self, username, hashed_password, hashed_answer, encrypted_password, encrypted_question,
+                               salt) -> bool:
+        self.cur.execute(
+            f"INSERT INTO UserSecurity (username, hashed_password, hashed_answer, encrypted_password, encrypted_question, salt) VALUES (?, ?, ?, ?, ?, ?)",
+            (username, hashed_password, hashed_answer, encrypted_password, encrypted_question, salt))
+        try:
+            self.con.commit()
+            return True
+        except:
+            return False
+
+    def close_cursor(self):
+        self.cur.close()
+
+    def reset_db(self):
+        apply_sql()
---
+++ src/main/python/MDPDatabase/security.py
@@ -0,0 +1,106 @@
+# author: Arthur De Neyer - GaecKo
+# last update: check github (https://github.com/GaecKo/MDPSaver)
+#           ==== ⚠ DISCLAIMER ⚠ ====
+# This part of the code concernes the security of the program. It uses verified and secured methods of famous modules (listed in the imports just below). 
+# In case of any suspicion on the system reliability and security, please open a new Issue on github. 
+
+import sys, os, base64
+from Crypto.Hash import SHA256
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def load_key(password, salt) -> bytes:
+    password = password.encode()
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=380000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    return key
+
+def encrypt(key: bytes, to_encrypt: str) -> str:
+    f = Fernet(key)
+    token = f.encrypt(to_encrypt.encode())
+    return token.decode()
+
+
+def decrypt(key: bytes, to_decrypt: str) -> str:
+    f = Fernet(key)
+    decrypted = f.decrypt(to_decrypt.encode())
+    return decrypted.decode()
+
+
+def try_decrypt(key, to_decrypt):
+    """
+    if okay: return true, if not: return False
+    """
+    try:
+        f = Fernet(key)
+        decrypted = f.decrypt(to_decrypt.encode())
+        return True
+    except:
+        return False
+
+def encrypt_extern_password(password:str, to_encrypt: str, salt) -> str:
+
+    password = password.encode()
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=380000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    token = f.encrypt(to_encrypt.encode())
+    return token.decode()
+
+def decrypt_extern_password(password:str, to_decrypt: str, salt) -> str:
+
+    password = password.encode()
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=380000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    decrypted = f.decrypt(to_decrypt.encode())
+    return decrypted.decode()
+
+
+
+def hashing(to_hash):
+    return SHA256.new(data=to_hash.encode()).hexdigest()
+
+def low_hash(string):
+    """
+    Not good hash !!
+    """
+    def to_32(value):
+        """
+        Fonction interne utilisée par hashing.
+        Convertit une valeur en un entier signé de 32 bits.
+        Si 'value' est un entier plus grand que 2 ** 31, il sera tronqué.
+
+        :param (int) value: valeur du caractère transformé par la valeur de hachage de cette itération
+        :return (int): entier signé de 32 bits représentant 'value'
+        """
+        value = value % (2 ** 32)
+        if value >= 2**31:
+            value = value - 2 ** 32
+        value = int(value)
+        return value
+
+    if string:
+        x = ord(string[0]) << 7
+        m = 1000003
+        for c in string:
+            x = to_32((x*m) ^ ord(c))
+        x ^= len(string)
+        if x == -1:
+            x = -2
+        return str(x)
+    return ""

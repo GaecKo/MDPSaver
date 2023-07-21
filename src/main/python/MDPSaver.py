8a82a0b440273777c40acee657dea694be42f970
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-07-21 18:27:23 +0200

    `FE`: New refresh support for Jinja templating

--- src/main/python/MDPSaver.py
+++ src/main/python/MDPSaver.py
@@ -12,26 +12,36 @@ class MDPSaver(QMainWindow):
     def __init__(self, debug=False):
         super().__init__()
 
-        self.bridge = Bridge() # Custom bridge class (controller)
+        self.bridge = Bridge()  # Custom bridge class (controller)
         self.debug = debug
 
-        # Launch UI
+        # Prepare UI
         self.__load_pages_path__()
         self.__init_ui__()
-        self.__load_ui__()
         self.__setup_connections__()
+
         if debug:
             self.__init_debug__()
+
+        # Launch UI
+        self.__launch_ui__()
 
     def __load_pages_path__(self):
         # Get the path to the current script and construct the HTML file path
         current_dir = os.path.dirname(os.path.abspath(__file__))
 
-        self.startup_path = os.path.join(current_dir, "../views/startup.html")
-        self.login_path = os.path.join(current_dir, "../views/login.html")
-        self.recovery_path = os.path.join(current_dir, "../views/recovery.html")
-        self.app_path = os.path.join(current_dir, "../views/app.html")
+        # raw HTML files, used to store jinja templated HTML for debug
+        if self.debug:
+            self.startup_path = os.path.join(current_dir, "../views/startup.html")
+            self.login_path = os.path.join(current_dir, "../views/login.html")
+            self.recovery_path = os.path.join(current_dir, "../views/recovery.html")
+            self.app_path = os.path.join(current_dir, "../views/app.html")
 
+        # jinja templates
+        self.startup_jinja = os.path.join(current_dir, "../views/startup.jinja2")
+        self.login_jinja = os.path.join(current_dir, "../views/login.jinja2")
+        self.recovery_jinja = os.path.join(current_dir, "../views/recovery.jinja2")
+        self.app_jinja = os.path.join(current_dir, "../views/app.jinja2")
 
     def __init_ui__(self):
         # Initialize UI with the WebView, Channel, ...
@@ -46,7 +56,7 @@ class MDPSaver(QMainWindow):
 
         channel.registerObject("bridge", self.bridge)
 
-    def __load_ui__(self):
+    def __launch_ui__(self):
         # Choose initial page to load
         if self.bridge.getNumberOfUser() == 0:
             self.__load_startup__()
@@ -56,6 +66,7 @@ class MDPSaver(QMainWindow):
     def __setup_connections__(self):
         # Startup Signals
         self.bridge.successful_startup.connect(self.__load_app__)
+        self.bridge.back_login_startup.connect(self.__load_login__)
 
         # Login Signals
         self.bridge.successful_login.connect(self.__load_app__)
@@ -69,41 +80,87 @@ class MDPSaver(QMainWindow):
         self.bridge.cancel_recovery.connect(self.__load_login__)
 
     def __init_debug__(self):
-        def reloadPage():
-            # Reload the web view when the HTML file changes
-            self.web_view.reload()
+        # Watch for changes in the jinja files and update the html files
 
-        self.file_watcher = QFileSystemWatcher([self.startup_path, self.login_path, self.recovery_path, self.app_path], self)
-        self.file_watcher.fileChanged.connect(reloadPage)
+        self.startup_watcher = QFileSystemWatcher([self.startup_jinja], self)
+        self.login_watcher = QFileSystemWatcher([self.login_jinja], self)
+        self.recovery_watcher = QFileSystemWatcher([self.recovery_jinja], self)
+        self.app_watcher = QFileSystemWatcher([self.app_jinja], self)
+
+        self.startup_watcher.fileChanged.connect(self.__load_startup__)
+        self.login_watcher.fileChanged.connect(self.__load_login__)
+        self.recovery_watcher.fileChanged.connect(self.__load_recovery__)
+        self.app_watcher.fileChanged.connect(self.__load_app__)
+
+        # Create debug html files:
+        open(self.startup_path, "w").write("")
+        open(self.login_path, "w").write("")
+        open(self.recovery_path, "w").write("")
+        open(self.app_path, "w").write("")
+
+    def __write_jinja__(self, html_path, html):
+        with open(html_path, "w") as f:
+            f.write(html)
+
+    def __remove_debug__(self):
+        # delete html files
+        os.remove(self.app_path)
+        os.remove(self.login_path)
+        os.remove(self.recovery_path)
+        os.remove(self.startup_path)
 
     def __load_startup__(self):
-        # TODO: load page using Jinja2
         # TODO: if app has usernames, button for login
-        self.web_view.load(QUrl.fromLocalFile(self.startup_path))
+        template = Template(open(self.startup_jinja).read())
+        html = template.render(nbr_user=self.bridge.getNumberOfUser())
 
-    def __load_login__(self, from_failed= False):
-        # TODO: load page using Jinja2
+        if self.debug:
+            self.__write_jinja__(self.startup_path, html)
 
-        template = Template(open(self.login_path).read())
+            self.web_view.load(QUrl.fromLocalFile(self.startup_path))
+        else:
+            self.web_view.setHtml(html)
 
-        self.web_view.setHtml(template.render(usernames=self.bridge.get_usernames(), from_failed=from_failed))
+    def __load_login__(self, from_failed=False):
 
+        template = Template(open(self.login_jinja).read())
+        html = template.render(usernames=self.bridge.get_usernames(), from_failed=from_failed)
 
-    def __load_recovery__(self, from_failed= False):
+        if self.debug:
+            self.__write_jinja__(self.login_path, html)
+            self.web_view.load(QUrl.fromLocalFile(self.login_path))
+        else:
+            self.web_view.setHtml(html)
 
-        # question = self.bridge.get_personnal_question() # XXX: Using templating or asking from JS?
+    def __load_recovery__(self):
 
-        template = Template(open(self.recovery_path).read())
-
-        self.web_view.setHtml(template.render(usernames=self.bridge.get_usernames(), selected_username=self.bridge.username))
+        template = Template(open(self.recovery_jinja).read())
+        html = template.render(usernames=self.bridge.get_usernames(), selected_username=self.bridge.username)
+        if self.debug:
+            self.__write_jinja__(self.recovery_path, html)
+            self.web_view.load(QUrl.fromLocalFile(self.recovery_path))
+        else:
+            self.web_view.setHtml(html)
 
     def __load_app__(self):
-        # TODO: load page using Jinja2
-        self.web_view.load(QUrl.fromLocalFile(self.app_path))
+
+        template = Template(open(self.app_jinja).read())
+        html =  template.render() # to complete further on
+        if self.debug:
+            self.__write_jinja__(self.app_path, html)
+            self.web_view.load(QUrl.fromLocalFile(self.app_path))
+        else:
+            self.web_view.setHtml(html)
+
 
     def closeEvent(self, event):
         # Stop monitoring the file for changes when the window is closed
         if self.debug:
-            self.file_watcher.removePaths(self.file_watcher.files())
-        event.accept()
+            self.startup_watcher.removePaths(self.startup_watcher.files())
+            self.login_watcher.removePaths(self.login_watcher.files())
+            self.recovery_watcher.removePaths(self.recovery_watcher.files())
+            self.app_watcher.removePaths(self.app_watcher.files())
 
+            self.__remove_debug__()
+
+        event.accept()
